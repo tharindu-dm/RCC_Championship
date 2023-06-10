85e531e09fc629586248b8aaaa5202454ec9ac67
Tharindu D Munasinghe <85168846+tharindu-dm@users.noreply.github.com>
2023-06-10 16:33:49 +0530

    Add files via upload

---
+++ src/main/java/com/mycompany/rcchampionship/RCChampionship.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.rcchampionship;
+
+/**
+ *
+ * @author thari
+ */
+public class RCChampionship {
+
+    public static void main(String[] args) {
+        new MainUI().setVisible(true);
+              
+    }
+}
